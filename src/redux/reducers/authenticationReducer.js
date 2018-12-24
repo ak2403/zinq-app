b7ceb6f698d2d52cd27ec4c98e7b0fff847aa734
ak2403 <arunpasupathi24@gmail.com>
2018-12-24 15:35:57 +1100

    File Structure added.

---
+++ src/redux/reducers/authenticationReducer.js
@@ -0,0 +1,10 @@
+let initialState = {
+
+}
+
+export default function AuthenticationReducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
