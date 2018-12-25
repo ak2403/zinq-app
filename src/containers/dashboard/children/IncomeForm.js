6d40565352ea24f78792014588d841df5de43207
ak2403 <arunpasupathi24@gmail.com>
2018-12-25 20:51:59 +1100

    Validations for the forms

--- src/containers/dashboard/children/IncomeForm.js
+++ src/containers/dashboard/children/IncomeForm.js
@@ -13,7 +13,39 @@ class IncomeForm extends Component {
             expense: 0,
             partner_income: 0,
             partner_expense: 0
+        },
+        validations: {
+            income: false,
+            expense: false
         }
+    }
+
+    componentDidMount = () => {
+        let { type, income } = this.props
+        let validations = {
+            income: false,
+            expense: false
+        }
+        let amount_data = {
+            income: income.income || 0,
+            expense: income.expense || 0
+        }
+
+        if (type === 'couple') {
+            validations = Object.assign({}, validations, {
+                partner_income: false,
+                partner_expense: false
+            })
+            amount_data = Object.assign({}, amount_data, {
+                partner_income: income.partner_income || 0,
+                partner_expense: income.partner_expense || 0
+            })
+        }
+
+        this.setState({
+            validations,
+            amount_data
+        })
     }
 
     changeValue = (name, value) => {
@@ -25,45 +57,81 @@ class IncomeForm extends Component {
     }
 
     onSubmit = () => {
-        let { amount_data } = this.state
-        this.props.submitIncome(amount_data)
+        let { amount_data, validations } = this.state
+        let is_valid = true
+
+        for (let key in amount_data) {
+            if (amount_data[key] === 0) {
+                is_valid = false
+                validations[key] = true
+            } else {
+                validations[key] = false
+            }
+        }
+
+        if (is_valid) {
+            this.props.submitIncome(amount_data)
+        }
+        this.setState({
+            validations
+        })
     }
 
     render() {
-        let { amount_data } = this.state
+        let { amount_data, validations } = this.state
         let { type } = this.props
 
-        let total_income = amount_data.income + amount_data.partner_income
-        let total_expense = amount_data.expense + amount_data.partner_expense
+        let total_income = amount_data.income + (amount_data.partner_income || 0)
+        let total_expense = amount_data.expense + (amount_data.partner_expense || 0)
 
         return (<div className="credit-income">
-            Total Income {total_income}
-            Total Expense {total_expense}
+            <div className="credit-header">
+                <div className="credit">
+                    <h3>Total Income</h3>
+                    {total_income}
+                </div>
+                <div className="credit">
+                    <h3>Total Expense</h3>
+                    {total_expense}
+                </div>
+            </div>
             <Form onSubmit={this.onSubmit}>
-                <Form.Field inline className="form-element">
+                <Form.Field inline className={`form-element ${validations['income'] && 'error'}`}>
                     <label>Your Income</label>
-                    <Input placeholder='Your Income' onChange={e => this.changeValue('income', e.target.value)} />
+                    <Input 
+                        placeholder='Your Income' 
+                        value={amount_data.income}
+                        onChange={e => this.changeValue('income', e.target.value)} />
                 </Form.Field>
 
-                <Form.Field inline className="form-element">
+                <Form.Field inline className={`form-element ${validations['expense'] && 'error'}`}>
                     <label>Your Expense</label>
-                    <Input placeholder='Your Expense' onChange={e => this.changeValue('expense', e.target.value)} />
+                    <Input 
+                        placeholder='Your Expense' 
+                        value={amount_data.expense}
+                        onChange={e => this.changeValue('expense', e.target.value)} />
                 </Form.Field>
                 {type === 'couple' ?
                     <React.Fragment>
-                        <Form.Field inline className="form-element">
+                        <Form.Field inline className={`form-element ${validations['partner_income'] && 'error'}`}>
                             <label>Your Partner Income</label>
-                            <Input placeholder='Your Partner Income' onChange={e => this.changeValue('partner_income', e.target.value)} />
+                            <Input 
+                            placeholder='Your Partner Income' 
+                            value={amount_data.partner_income}
+                            onChange={e => this.changeValue('partner_income', e.target.value)} />
                         </Form.Field>
 
-                        <Form.Field inline className="form-element">
+                        <Form.Field inline className={`form-element ${validations['partner_expense'] && 'error'}`}>
                             <label>Your Partner Expense</label>
-                            <Input placeholder='Your Partner Expense' onChange={e => this.changeValue('partner_expense', e.target.value)} />
+                            <Input 
+                                placeholder='Your Partner Expense' 
+                                value={amount_data.partner_expense}
+                                onChange={e => this.changeValue('partner_expense', e.target.value)} />
                         </Form.Field>
                     </React.Fragment>
                     : ''}
                 <Form.Field inline className="form-button">
-                    <span onClick={()=>this.props.goback('select')}>Back</span>
+                    <span onClick={() => this.props.goback('select')}>Back</span>
                     <Button type='submit'>Next</Button>
                 </Form.Field>
             </Form>
@@ -76,7 +144,8 @@ const mapDispatchToProps = dispatch => bindActionCreators({ submitIncome, goback
 const mapStateToProps = props => {
     let { dashboard } = props
     return {
-        type: dashboard.type
+        type: dashboard.type,
+        income: dashboard.income
     }
 }
 
--- src/containers/dashboard/children/PropertyForm.js
+++ src/containers/dashboard/children/PropertyForm.js
@@ -3,7 +3,7 @@ import { Form, Input, Button } from 'semantic-ui-react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import { submitProperty } from '../../../redux/actions/dashboard_actions'
+import { submitProperty, goback } from '../../../redux/actions/dashboard_actions'
 
 class PropertyForm extends Component {
 
@@ -11,42 +11,67 @@ class PropertyForm extends Component {
         property_data: {
             property_value: 0,
             deposit_value: 0
+        },
+        is_error: {
+            property_value: false,
+            deposit_value: false
         }
     }
 
     changeValue = (name, value) => {
         let { property_data } = this.state
-        property_data[name] = Number(value)
+        property_data[name] = Number(value) || 0
         this.setState({
             property_data
         })
     }
 
     onSubmit = () => {
-        let {property_data}=this.state
-        this.props.submitProperty(property_data)
+        let { property_data, is_error } = this.state
+        let is_valid = true
+
+        for (let key in property_data) {
+            if (property_data[key] === 0) {
+                is_valid = false
+                is_error[key] = true
+            } else {
+                is_error[key] = false
+            }
+        }
+
+        if (is_valid) {
+            this.props.submitProperty(property_data)
+        }
+        this.setState({
+            is_error
+        })
+
     }
 
     render() {
-        let { property_data } = this.state
-
-        return (<div>
+        let { property_data, is_error } = this.state
+console.log(is_error)
+        return (<div className="credit-income">
             <Form onSubmit={this.onSubmit}>
-                <Form.Field inline>
+                <Form.Field inline className={`form-element ${is_error['property_value'] && 'error'}`}>
                     <label>Your Property Value</label>
                     <Input placeholder='property Value' onChange={e => this.changeValue('property_value', e.target.value)} />
                 </Form.Field>
 
-                <Form.Field inline>
+                <Form.Field inline className={`form-element ${is_error['deposit_value'] && 'error'}`}>
                     <label>Deposit</label>
                     <Input placeholder='Your deposit' onChange={e => this.changeValue('deposit_value', e.target.value)} />
                 </Form.Field>
-                <Button type='submit'>Next</Button>
+
+                <Form.Field inline className="form-button">
+                    <span onClick={() => this.props.goback('income')}>Back</span>
+                    <Button type='submit'>Next</Button>
+                </Form.Field>
             </Form>
         </div>)
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ submitProperty }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ submitProperty, goback }, dispatch)
 
 export default connect(null, mapDispatchToProps)(PropertyForm)
